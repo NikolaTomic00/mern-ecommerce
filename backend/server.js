40641c0358a9159829d2a7e32ee330453cf1883e
NikolaTomic00 <tomicnikolaftn@gmail.com>
2026-05-16 01:17:03 +0200

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootDir = path.resolve();
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(rootDir, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(rootDir, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
+});
